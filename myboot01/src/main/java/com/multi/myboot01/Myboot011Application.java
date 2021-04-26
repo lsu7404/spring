60ea0288532bec5b39b28bbe6f788529eae8dd84
이선욱 <@DESKTOP-PMVSEBS>
2021-04-26 15:29:37 +0900

    Initial Upload

---
+++ myboot01/src/main/java/com/multi/myboot01/Myboot011Application.java
@@ -0,0 +1,13 @@
+package com.multi.myboot01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myboot011Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myboot011Application.class, args);
+	}
+
+}
---
+++ myboot01/src/test/java/com/multi/myboot01/Myboot011ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multi.myboot01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Myboot011ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
